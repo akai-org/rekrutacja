8104c4fdc72c917994f94d1edf1087f30baa69a9
Robert Banaszak <robert.banaszak1997@gmail.com>
2019-10-07 19:23:10 +0200

    task frontend

---
+++ html-css-js/README.md
@@ -0,0 +1,8 @@
+<h3>AKAI - Zadanie Frontend</h3>
+<p>Upiększ ten formularz!<p>
+
+<p>Do dyspozycji masz trzy pliki odpowiedzialne za poszczególne etapy zadania: html, css i javascript.
+W każdym z tych plików znajduje się instrukcja zawierająca informację na temat tego, co należy wykonać.
+Zacznij od pliku index.html, przeanalizuj go, a następnie przejrzyj plik .css i .js. </p>
+
+<h3>Powodzenia!</h3>
---
+++ html-css-js/js/validation.js
@@ -0,0 +1,11 @@
+/*
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    AKAI Frontend Task - Javascript
+
+    W tym zadaniu postaraj się zaimplementować metody, które sprawdzą, czy dane wprowadzone
+    do formularza są poprawne. Przykładowo: czy imię i nazwisko zostało wprowadzone.
+    Możesz rozwinąć walidację danych tak bardzo, jak tylko zapragniesz.
+
+    Powodzenia!
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
